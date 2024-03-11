d5de23e67a663ec6a8f2a7e70029664e7a7cc42a
NareshMatta <mattanareshbabu1998@gmail.com>
2024-03-11 10:56:57 +0530

    npncommit

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div>
       <center>
-     <h3> Header is a top of the table</h3>
+     <h3> Header is a top of the table on the </h3>
       </center>
        </div>
   )
